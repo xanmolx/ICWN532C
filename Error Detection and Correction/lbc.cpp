12591e10335c30b7449d0398e3d8ab6a3870708c
xanmolx <anmolsinghsethi@yahoo.com>
2018-08-14 15:24:32 +0530

    paper added from internet

---
+++ Error Detection and Correction/lbc.cpp
@@ -0,0 +1,15 @@
+//Linear Block Code
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, k;
+    cout << "Enter the number of Message Bits (n) ";
+    cin >> n;
+    cout << endl;
+
+    cout << "Enter the NUmber of Parity bits (k) ";
+    cin >> k;
+    cout << endl;
+}
